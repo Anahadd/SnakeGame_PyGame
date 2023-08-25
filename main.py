b032afde88bfd62b9e1aa684de0d95169d350c54
Anahad Dhaliwal <142919181+Anahadd@users.noreply.github.com>
2023-08-24 20:15:15 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pygame
 from pygame.locals import *
 from fruit import Fruits
+from blocks import Block
+import time
 
+# game
 pygame.init()
 rW = 906
 rH = 608
@@ -10,14 +13,20 @@ clock = pygame.time.Clock()
 pygame.display.set_caption("Snake Game || Anahad")
 white = (255, 255, 255)
 
+# variables
 x = 50
 y = 50
 playerRect = Rect(x, y, 32, 32)
 running = True
+speed = 0.2
+fruit = Fruits(200, 200)
+last_key = ""
 
+# returns the rectangle
 def drawPlayer(playerRect):
     return pygame.draw.rect(screen, (0, 0, 255), playerRect)
 
+# checks for boundaries (if player moves out of the screen)
 def checkBoundaries(x, y):
     if x <= 0 or x >= rW or y <= 0 or y >= rH:
         return False
@@ -25,25 +34,35 @@ def checkBoundaries(x, y):
     else:
         return True
 
-speed = 0.2
+blocks = [[0] * 4 for i in range(100)]
 
-fruit = Fruits(200, 200)
+for i in range(0, 100):
+    blocks[i] = Block(x, y)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            break
 
     keysPressed = pygame.key.get_pressed()
 
     if keysPressed[pygame.K_LEFT]:
-        x -= speed + (fruit.returnCount() * 0.05)
+        last_key = "L"
     elif keysPressed[pygame.K_RIGHT]:
-        x += speed + (fruit.returnCount() * 0.05)
+        last_key = "R"
     elif keysPressed[pygame.K_UP]:
-        y -= speed + (fruit.returnCount() * 0.05)
+        last_key = "U"
     elif keysPressed[pygame.K_DOWN]:
-        y += speed + (fruit.returnCount() * 0.05)
+        last_key = "D"
+
+    if last_key == "L":
+        x -= speed + (fruit.returnCount() * 0.025)
+    elif last_key == "R":
+        x += speed + (fruit.returnCount() * 0.025)
+    elif last_key == "U":
+        y -= speed + (fruit.returnCount() * 0.025)
+    elif last_key == "D":
+        y += speed + (fruit.returnCount() * 0.025)
 
     screen.fill(white)
 
@@ -53,6 +72,15 @@ while running:
 
     fruit.checkCollision(playerRect)
     fruit.drawFruit()
+
+    for i in range(0, fruit.returnCount()):
+
+        if i != 0:
+            blocks[i].drawBlocks()
+            blocks[i].adjustDirection(last_key, blocks[i-1].getX(), blocks[i-1].getY())
+        else:
+            blocks[0].drawBlocks()
+            blocks[0].adjustDirection(last_key, x, y)
 
     pygame.display.flip()
 
