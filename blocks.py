6f11b385c80ee97d8a47ed221ac726288cfe58fe
Anahad Dhaliwal <142919181+Anahadd@users.noreply.github.com>
2023-08-25 12:53:13 -0400

    Update blocks.py

--- blocks.py
+++ blocks.py
@@ -1,32 +1,40 @@
 import pygame
 from pygame.locals import *
 
-rW = 906
-rH = 608
-screen = pygame.display.set_mode((rW, rH))
-black = (0, 0, 0)
-
 class Block:
     def __init__(self, x, y):
         self.x = x
         self.y = y
+        self.rect = pygame.Rect(self.x, self.y, 32, 32)
+        self.color = (255, 0, 0)
 
-    def drawBlocks(self):
-        return pygame.draw.rect(screen, black, Rect(self.x, self.y, 32, 32))
+    def draw(self, screen):
+        return pygame.draw.rect(screen, self.color, self.rect)
 
-    def adjustDirection(self, last_key, xPos, yPos):
-        if last_key == "L":
-            self.x = xPos + 32
-            self.y = yPos
-        elif last_key == "R":
-            self.x = xPos - 32
-            self.y = yPos
-        elif last_key == "D":
-            self.y = yPos - 32
-            self.x = xPos
-        elif last_key == "U":
-            self.y = yPos + 32
-            self.x = xPos
+    def move(self, direction, distance=32):
+        if direction == "L":
+            self.x -= distance
+        elif direction == "R":
+            self.x += distance
+        elif direction == "U":
+            self.y -= distance
+        elif direction == "D":
+            self.y += distance
+        self.update_rect()
+
+    def set_position(self, x, y):
+        self.x = x
+        self.y = y
+        self.update_rect()
+
+    def update_rect(self):
+        self.rect = (self.x, self.y)
+
+    def get_rect(self):
+        return self.rect
+
+    def get_position(self):
+        return self.x, self.y
 
     def getX(self):
         return self.x
