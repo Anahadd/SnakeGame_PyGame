165076a7c01a75452b61688d3a6dd246b0982769
Anahad Dhaliwal <142919181+Anahadd@users.noreply.github.com>
2023-08-25 12:53:00 -0400

    Update fruit.py

--- fruit.py
+++ fruit.py
@@ -4,7 +4,6 @@ import random
 
 rW = 906
 rH = 608
-screen = pygame.display.set_mode((rW, rH))
 
 class Fruits:
     def __init__(self, x, y):
@@ -13,16 +12,22 @@ class Fruits:
         self.rect = Rect(self.x, self.y, 32, 32)
         self.count = 0
 
-    def drawFruit(self):
-        self.rect = Rect(self.x, self.y, 32, 32)
+    def drawFruit(self, screen):
+        self.rect.topleft = (self.x, self.y)
         return pygame.draw.rect(screen, (255, 0, 0), self.rect)
 
     def checkCollision(self, p):
         collide = pygame.Rect.colliderect(p, self.rect)
         if collide:
-            self.x = random.randint(100, rW - 100)
-            self.y = random.randint(100, rH - 100)
+            self.reposition()
             self.count += 1
+            return True
+        return False
+
+    def reposition(self):
+        self.x = random.randint(0, rW - 32)
+        self.y = random.randint(0, rH - 32)
 
     def returnCount(self):
         return self.count
+
