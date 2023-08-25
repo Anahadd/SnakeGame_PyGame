df398919d7609a2a0f98540a1b98278e12f4799e
Anahad Dhaliwal <142919181+Anahadd@users.noreply.github.com>
2023-08-25 17:48:18 -0400

    Update main.py
    fix collision w self by adding bfs if there is blocks in its own way

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import pygame
 from pygame.locals import *
 from fruit import Fruits
 from blocks import Block
+import time
+import numpy as np
+
 
 # game
 pygame.init()
@@ -19,34 +22,31 @@ playerRect = Rect(x, y, 32, 32)
 running = True
 speed = 32
 last_key = ""
-fruit = Fruits(320, 320)
+fruit = Fruits(192, 288)
 snake_blocks = [Block(x, y)]
 
 # q learning parameters
 updateCoordinates = ()
 updateCoordinatesBlocks = [[0] * 1 for i in range(625)]
 updateCoordinatesFruit = ()
-grid = []
-grid_state = []
 
-# for future use - q learning
-for i in range(26):
-    for k in range(26):
-        grid.append((32 * i, 32 * k))
-        grid_state.append("empty")
+grid_state = np.full((25, 25), "empty", dtype=object)
+
 
 # draws player
 def drawPlayer(block):
     pygame.draw.rect(screen, (0, 255, 0), block.get_rect())
 
-def checkBoundaries(block):
+
+def checkBoundaries(block, incX, incY):
     # boundary checking
-    x, y = block.getX(), block.getY()
-    if x <= 0 or x >= rW or y <= 0 or y >= rH:
+    x, y = block.getX() + incX, block.getY() + incY
+    if x < 0 or x > rW or y < 0 or y > rH:
         print("Game Over! You went out of bounds.")
         return False
     else:
         return True
+
 
 def draw_grid():
     # draws the grid lines
@@ -55,28 +55,98 @@ def draw_grid():
     for y in range(0, rH, 32):
         pygame.draw.line(screen, (0, 0, 0), (0, y), (rW, y))
 
-# updates grid state
+def pixel_to_grid(x, y):
+    return x // 32, y // 32
+
 def updateGrid(updateCoordinates, updateCoordinatesBlocks, updateCoordinatesFruit):
-    for i in range(0, len(grid)):
-        current = grid[i]
-        grid_state[i] = "empty"
-        if current == updateCoordinates:
-            grid_state[i] = "head of snake"
-        for k in updateCoordinatesBlocks:
-            if current == k:
-                grid_state[i] = "snake"
-        if current == updateCoordinatesFruit:
-            grid_state[i] = "food"
+    grid_state[:, :] = "empty"
 
-    '''
-    # prints out grid state (will not be needed)
-    for row in range(25):
-        for col in range(25):
-            print(f"{grid_state[row * 25 + col]:4}", end=" ")
-        print()
+    head_x, head_y = pixel_to_grid(*updateCoordinates)
+    grid_state[head_x][head_y] = "head of snake"
 
-    print("\n\n\n")
-    '''
+    for b in updateCoordinatesBlocks:
+        block_x, block_y = pixel_to_grid(*b)
+        grid_state[block_x][block_y] = "snake"
+
+    fruit_x, fruit_y = pixel_to_grid(*updateCoordinatesFruit)
+    grid_state[fruit_x][fruit_y] = "food"
+
+
+'''def checkDir(x, y, dir):
+    row = x // 32
+    col = y // 32
+
+    count = 0
+
+    if dir == "L":
+        for i in range(0, col):
+            if grid_state[row][i] == "empty":
+                count += 1
+
+        if count == col:
+            return True
+            print("clear")
+        else:
+            return False
+
+    elif dir == "R":
+        for i in range(col + 1, 25):
+            if grid_state[row][i] == "empty":
+                count += 1
+
+        if count == 24 - col:
+            return True
+            print("clear")
+        else:
+            return False
+
+    elif dir == "U":
+        for i in range(0, row):
+            if grid_state[i][col] == "empty":
+                count += 1
+        if count == row:
+            return True
+            print("clear")
+        else:
+            return False
+
+    elif dir == "D":
+        for i in range(row + 1, 25):
+            if grid_state[i][col] == "empty":
+                count += 1
+        if count == 24 - row:
+            return True
+            print("clear")
+        else:
+            return False
+'''
+def pathToFood(x_one, x_two, y_one, y_two, last_key):
+    # manhattan distance
+    total_x = x_two - x_one
+    total_y = y_two - y_one
+
+    dX = int(total_x / 32)
+    dY = int(total_y / 32)
+
+    if dX != 0:
+        if dX > 0 or dX < rW:
+            if dX > 0:
+                last_key = "L"
+                return "R"
+            else:
+                last_key = "R"
+                return "L"
+    elif dY != 0:
+        if dY > 0 or dY < rH:
+            if dY > 0:
+                last_key = "U"
+                return "D"
+            else:
+                last_key = "D"
+                return "U"
+    else:
+        return 'S'
+
 
 while running:
     for event in pygame.event.get():
@@ -99,14 +169,16 @@ while running:
     # set parameters for snake head (x, y)
     head_x, head_y = snake_blocks[0].getX(), snake_blocks[0].getY()
 
+    d = pathToFood(head_x, fruit.getX(), head_y, fruit.getY(), last_key)
+
     # directions
-    if last_key == "L":
+    if d == "L" and checkBoundaries(snake_blocks[0], -32, 0) == True:
         head_x -= speed
-    elif last_key == "R":
+    elif d == "R" and checkBoundaries(snake_blocks[0], 32, 0) == True:
         head_x += speed
-    elif last_key == "U":
+    elif d == "U" and checkBoundaries(snake_blocks[0], 0, -32) == True:
         head_y -= speed
-    elif last_key == "D":
+    elif d == "D" and checkBoundaries(snake_blocks[0], 0, 32) == True:
         head_y += speed
 
     # create new head
@@ -122,19 +194,11 @@ while running:
     # bg
     screen.fill((0, 0, 255))
 
-    updateCoordinatesFruit = (fruit.getX(), fruit.getY())
-
-    # setting grid parameters
-    updateGrid(updateCoordinates, updateCoordinatesBlocks, updateCoordinatesFruit)
-
     # boundary check
-    running = checkBoundaries(snake_blocks[0])
+    running = checkBoundaries(snake_blocks[0], 0, 0)
 
-    # update pos
-    updateCoordinates = (x, y)
     for block in snake_blocks:
         drawPlayer(block)
-        updateCoordinatesBlocks.append((block.getX(), block.getY()))
 
         # check if snake hit itself
         if block != snake_blocks[0]:
@@ -142,6 +206,12 @@ while running:
             if collide:
                 running = False
                 print("Collision With Self")
+                print(snake_blocks[1].getX(), snake_blocks[1].getY())
+
+    snake_head_coord = (snake_blocks[0].getX(), snake_blocks[0].getY())
+    snake_body_coords = [(block.getX(), block.getY()) for block in snake_blocks[1:]]
+    fruit_coord = (fruit.getX(), fruit.getY())
+    updateGrid(snake_head_coord, snake_body_coords, fruit_coord)
 
     # draw gui
     fruit.drawFruit(screen)
