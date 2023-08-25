c3bf45526a0f80b67717de2b372ce584c70dfe49
Anahad Dhaliwal <142919181+Anahadd@users.noreply.github.com>
2023-08-25 12:52:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,77 +18,76 @@ x = 50
 y = 50
 playerRect = Rect(x, y, 32, 32)
 running = True
-speed = 0.2
+speed = 10
 fruit = Fruits(200, 200)
 last_key = ""
 
-# returns the rectangle
-def drawPlayer(playerRect):
-    return pygame.draw.rect(screen, (0, 0, 255), playerRect)
+snake_blocks = [Block(x, y)]
 
-# checks for boundaries (if player moves out of the screen)
-def checkBoundaries(x, y):
+def drawPlayer(block):
+    pygame.draw.rect(screen, (0, 255, 0), block.get_rect())
+
+
+def checkBoundaries(block):
+    x, y = block.getX(), block.getY()
     if x <= 0 or x >= rW or y <= 0 or y >= rH:
-        return False
         print("Game Over! You went out of bounds.")
+        return False
     else:
         return True
 
-blocks = [[0] * 4 for i in range(100)]
-
-for i in range(0, 100):
-    blocks[i] = Block(x, y)
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            break
+            running = False
 
     keysPressed = pygame.key.get_pressed()
 
-    if keysPressed[pygame.K_LEFT]:
+    if keysPressed[pygame.K_LEFT] and last_key != "R":
         last_key = "L"
-    elif keysPressed[pygame.K_RIGHT]:
+    elif keysPressed[pygame.K_RIGHT] and last_key != "L":
         last_key = "R"
-    elif keysPressed[pygame.K_UP]:
+    elif keysPressed[pygame.K_UP] and last_key != "D":
         last_key = "U"
-    elif keysPressed[pygame.K_DOWN]:
+    elif keysPressed[pygame.K_DOWN] and last_key != "U":
         last_key = "D"
 
+    head_x, head_y = snake_blocks[0].getX(), snake_blocks[0].getY()
+
     if last_key == "L":
-        x -= speed + (fruit.returnCount() * 0.025)
+        head_x -= speed
     elif last_key == "R":
-        x += speed + (fruit.returnCount() * 0.025)
+        head_x += speed
     elif last_key == "U":
-        y -= speed + (fruit.returnCount() * 0.025)
+        head_y -= speed
     elif last_key == "D":
-        y += speed + (fruit.returnCount() * 0.025)
+        head_y += speed
+
+    new_head = Block(head_x, head_y)
+    snake_blocks.insert(0, new_head)
+
+    if fruit.checkCollision(new_head.get_rect()):
+        fruit.reposition()
+    else:
+        snake_blocks.pop()
 
     screen.fill(white)
 
-    playerRect = Rect(x, y, 32, 32)
-    drawPlayer(playerRect)
-    running = checkBoundaries(x, y)
+    for block in snake_blocks:
+        drawPlayer(block)
 
-    fruit.checkCollision(playerRect)
-    fruit.drawFruit()
+    running = checkBoundaries(snake_blocks[0])
 
-    for i in range(0, fruit.returnCount()):
-
-        if i != 0:
-            blocks[i].drawBlocks()
-            blocks[i].adjustDirection(last_key, blocks[i-1].getX(), blocks[i-1].getY())
-        else:
-            blocks[0].drawBlocks()
-            blocks[0].adjustDirection(last_key, x, y)
+    fruit.drawFruit(screen)
 
     font = pygame.font.Font("MoonbrightDemo-1GGn2.ttf", 32)
-    text = font.render("SCORE: " + str(fruit.returnCount()), True, (0, 0, 0), (255, 255, 255))
+    text = font.render("SCORE: " + str(len(snake_blocks) - 1), True, (0, 0, 0), (255, 255, 255))
     textRect = text.get_rect()
     textRect.center = (100, 100)
-
     screen.blit(text, textRect)
 
     pygame.display.flip()
+    clock.tick(15 + fruit.returnCount())
 
 pygame.quit()
